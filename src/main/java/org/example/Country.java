ad72d7ce9449ae109df7ce5d58a4d71bf96ca401
kajela <titoyut56@gmail.com>
2022-06-06 16:01:20 -0400

    add the initial files

--- src/main/java/org/example/Country.java
+++ src/main/java/org/example/Country.java
@@ -1,2 +1,29 @@
-package org.example;public class Country {
+package org.example;
+
+public class Country {
+    private String name;
+    private String city;
+    int num;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setName(String newName){
+        this.name = newName;
+    }
+    public void setCity(String newCity){
+        this.city = newCity;
+    }
+    public void setNum(int newNum){
+        this.num = newNum;
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,14 @@
-package org.example;public class Main {
+package org.example;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Country myObj = new Country();
+       // Country myObj = new Country();
+        myObj.setCity("queensland");
+        myObj.setName("australia");
+        myObj.setNum(2200);
+        System.out.println(myObj.getName());
+       System.out.println(myObj.getCity());
+       System.out.println(myObj.getNum());
     }
 }
